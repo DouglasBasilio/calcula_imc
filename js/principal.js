51e6517235e95cbdb0a80cb080685ceaa50b6c13
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-08-26 20:44:58 -0300

    criando paciente

--- js/principal.js
+++ js/principal.js
@@ -37,3 +37,46 @@ for(i = 0; i < pacientes.length; i++){
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+        var botaoAdicionar = document.querySelector("#adicionar-paciente");
+        botaoAdicionar.addEventListener("click", function(){
+        event.preventDefault(); // previno que o botão limpe os campos e recarregue a página
+
+        var form = document.querySelector("#form-adiciona"); //pego as tags do form
+        var nome = form.nome.value;
+        var peso = form.peso.value;
+        var altura = form.altura.value;
+        var gordura = form.gordura.value;
+
+/*         console.log(nome);
+        console.log(peso);
+        console.log(altura);
+        console.log(gordura); */
+
+        // crio uma variável e determino que ela é o elemento tr
+        var pacienteTr = document.createElement("tr");
+
+        // crio o elemento td usando
+        var nomeTd = document.createElement("td");
+        var pesoTd = document.createElement("td");
+        var alturaTd = document.createElement("td");
+        var gorduraTd = document.createElement("td");
+        var imcTd = document.createElement("td");
+
+        // altero o conteúdo da 'td'
+        nomeTd.textContent = nome;
+        pesoTd.textContent = peso;
+        alturaTd.textContent = altura;
+        gorduraTd.textContent = gordura;
+
+        // adiciono o elemento dentro de outro, no caso, o td dentro da tr
+        pacienteTr.appendChild(nomeTd);
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(alturaTd);
+        pacienteTr.appendChild(gorduraTd);
+
+        // agora crio a variável tabela para associar meu tr ao tbody
+        var tabela = document.querySelector("#tabela-pacientes");
+        tabela.appendChild(pacienteTr);
+    })
+
