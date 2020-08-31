13db15fdd3750e14c4ba52cff3a95e9bc46b417c
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-08-31 17:23:18 -0300

    criada conexão com api paciente

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,22 @@
+//
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Buscando pacientes");
+
+    var xhr = new XMLHttpRequest(); // é um objeto do JS, responsável por fazer requisições HTTP
+
+    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes"); // abrir a conexão
+
+    // escuta o evento LOAD e acessa os dados da resposta
+    xhr.addEventListener("load", function(){
+        console.log(xhr.responseText);
+    });
+
+    xhr.send(); // pega a requisição e envia
+
+});
+
+/* Dado que temos um XMLHttpRequest, precisamos configurar nossa requisição para dizer para qual servidor queremos enviá-la e também qual método HTTP devemos usar.
+
+Fazemos isto através do método .open() , passando o método e a url  */
